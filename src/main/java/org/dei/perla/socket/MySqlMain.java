0447c02e5a0e5f3958eb474742dfeabd90c238ed
francesco.filipazzi <francesco.filipazzi@FILIPAZZIF10.beta80group.it>
2017-05-12 10:27:06 +0200

    Database modification

--- src/main/java/org/dei/perla/socket/MySqlMain.java
+++ src/main/java/org/dei/perla/socket/MySqlMain.java
@@ -9,24 +9,31 @@ public class MySqlMain {
 	
 	
 	public static void main(String args[]){
-		Connection con;
-		Statement cmd;
+		Connection con = null;
+		Statement cmd = null;
 		
 		MySqlPerLa myPerla = new MySqlPerLa();
 		try {
 		con = myPerla.connect("jdbc:mysql://localhost/perla_database");
 		cmd = con.createStatement();
 		
-		String insertingQuery="INSERT INTO st_el (nome, cognome, eta)"
-				+ "VALUES (2, 3, 4);";
-		
-		cmd.executeUpdate(insertingQuery);
 		
 		
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		String insertingQuery="INSERT INTO st_el (nome, cognome, eta)"
+				+ "VALUES (2, 3, 4);";
+		
+		try {
+			cmd.executeUpdate(insertingQuery);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 	}
 
--- src/main/java/org/dei/perla/socket/XMLParser.java
+++ src/main/java/org/dei/perla/socket/XMLParser.java
@@ -9,6 +9,9 @@ import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class XMLParser {
 
@@ -31,7 +34,23 @@ public class XMLParser {
 			NodeList nList = doc.getElementsByTagName("entry");
 
 			System.out.println("----------------------------");
-
+			//Connection to database	
+			Connection con = null;
+			Statement cmd = null;
+			
+			MySqlPerLa myPerla = new MySqlPerLa();
+			
+			try {
+				con = myPerla.connect("jdbc:mysql://localhost/perla_database");
+				cmd = con.createStatement();
+				
+				
+				
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
 			for (int temp = 0; temp < nList.getLength(); temp++) {
 
 				Node nNode = nList.item(temp);
@@ -42,15 +61,49 @@ public class XMLParser {
 
 					Element eElement = (Element) nNode;
 
-					System.out.println("id : " + eElement.getAttribute("id"));
-					System.out.println("Title : " + eElement.getElementsByTagName("title").item(0).getTextContent());
-					System.out.println("Date : " + eElement.getElementsByTagName("updated").item(0).getTextContent());
-					System.out.println("Latitude : " + eElement.getElementsByTagName("geo:lat").item(0).getTextContent());
-					System.out.println("Longitude : " + eElement.getElementsByTagName("geo:long").item(0).getTextContent());
-					System.out.println("Summary : " + eElement.getElementsByTagName("summary").item(0).getTextContent());
+					//System.out.println("id : " + eElement.getAttribute("id"));
+					//System.out.println("Title : " + eElement.getElementsByTagName("title").item(0).getTextContent());
+					//System.out.println("Date : " + eElement.getElementsByTagName("updated").item(0).getTextContent());
+					//System.out.println("Latitude : " + eElement.getElementsByTagName("geo:lat").item(0).getTextContent());
+					//System.out.println("Longitude : " + eElement.getElementsByTagName("geo:long").item(0).getTextContent());
+					//System.out.println("Summary : " + eElement.getElementsByTagName("summary").item(0).getTextContent());
+					
+					String insertingQuery = "INSERT INTO ingv (identification, title, updated, latitude, longitude, summary)"
+							+ "VALUES ("
+							+ "'nicetry'," 
+							+ "'"+eElement.getElementsByTagName("title").item(0).getTextContent().replace("'", "-")+"', " 
+							+ "'"+eElement.getElementsByTagName("updated").item(0).getTextContent().replace("'", "-")+"', "
+							+ "'"+eElement.getElementsByTagName("geo:lat").item(0).getTextContent().replace("'", "-")+"', "
+							+ "'"+eElement.getElementsByTagName("geo:long").item(0).getTextContent().replace("'", "-")+"', "
+							+ "'"+eElement.getElementsByTagName("summary").item(0).getTextContent().replace("'", "-")+"');";
+					
+					String insertingQuery2 = "INSERT INTO ingv (identification, title, updated, latitude, longitude, summary)"
+							+ "VALUES (2, 3, 4, 5, 6, 7);";
+					
+					System.out.println(insertingQuery);
+					
+					try {
+						cmd.executeUpdate(insertingQuery);
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
 				}
 			}
 		    
 		  }
+	
+	/**
+	 * CREATE TABLE ingv (
+      id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+      identification VARCHAR(30) NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    updated VARCHAR(50),
+    latitude VARCHAR(50),
+    longitude VARCHAR(50),
+     summary VARCHAR(50)
+)
+	**/
 
 }
